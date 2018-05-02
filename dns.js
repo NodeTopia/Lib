5c50f2414477157fa4725152761a5fa6c66ca785
flybyme <mangoraft@gmail.com>
2018-05-02 07:21:13 -0500

    update dns to es6/await

--- dns.js
+++ dns.js
@@ -1,176 +1,223 @@
-var nconf = require('nconf');
-var tld = require('tldjs');
+const tld = require('tldjs');
+const restify = require('restify')
+const mongoose = require('nodetopia-model');
+const kue = require('nodetopia-kue');
 
-var mongoose = require('nodetopia-model');
-var kue = require('nodetopia-kue');
+const to = require('./to');
 
 var dns = module.exports = {
-	getZone : function(zone, organization, callback) {
+    getZone: function (zone, organization) {
+        return new Promise(async function (resolve, reject) {
+            let [err, dnsZone] = await to(mongoose.DNSZone.findOne({
+                zone: zone
+            }))
 
-		mongoose.DNSZone.findOne({
-			zone : zone
-		}, function(err, dnsZone) {
-			if (err) {
-				err.type = 'InternalError';
-				return callback(err);
-			}
+            if (err) {
+                err.type = 'InternalError';
+                reject(err)
+                return
+            }
 
-			if (!dnsZone) {
-				dnsZone = new mongoose.DNSZone({
-					organization : organization,
-					zone : zone
-				});
-			} else if (dnsZone.organization.toString() != organization.toString()) {
-				return callback(new Error('Zone used by another organization'));
-			}
-			callback(null, dnsZone);
+            if (!dnsZone) {
+                dnsZone = new mongoose.DNSZone({
+                    organization: organization,
+                    zone: zone
+                });
+            } else if (dnsZone.organization.toString() !== organization.toString()) {
+                return reject(new Error('Zone used by another organization'));
+            }
+            resolve(dnsZone);
+        })
+    },
+    validateRecord: function (records, name, type, data, priority, reject) {
+        for (let record of records) {
+            if (record.name === name && record.type === type && record.data === data) {
+                if (record.ttl !== ttl) {
+                    reject(new Error('dns record already exists ttl change?'));
+                    return false
+                } else if (record.priority !== priority) {
+                    reject(new Error('dns record already exists priority change?'));
+                    return false
+                } else {
+                    reject(new Error(`dns record already exists ${type}:${name}:'${data}`));
+                    return false
+                }
+            }
+        }
+        return true
+    },
+    add: function (_data) {
+        return new Promise(async function (resolve, reject) {
 
-		});
-	},
-	add : function(_data, callback) {
-		console.log(_data)
-		var organization = _data.organization;
+            let {
+                organization,
+                name,
+                type,
+                ttl = 3600,
+                priority,
+                data
+            } = _data
 
-		var name = _data.name.toLowerCase();
-		var type = _data.type.toUpperCase();
-		var ttl = _data.ttl || 3600;
-		var priority = _data.priority;
-		var data = _data.data;
-		var zone = tld.getDomain(name.replace('*.', ''));
+            name = name.toLowerCase();
+            type = type.toUpperCase();
+            let zone = tld.getDomain(name.replace('*.', ''));
 
-		if (type == 'MX' && priority == undefined) {
-			priority = 10;
-		}
+            if (type === 'MX' && priority === undefined) {
+                priority = 10;
+            }
 
-		dns.getZone(zone, organization, function(err, dnsZone) {
-			if (err) {
-				err.type = 'InternalError';
-				return callback(err);
-			}
+            let dnsZone
+            try {
+                dnsZone = await dns.getZone(zone, organization)
+            } catch (err) {
+                err.type = 'InternalError';
+                return reject(err);
+            }
 
-			if (dnsZone.isNew) {
-				return callback(new restify.errors.NotFoundError('zone does not exists'));
-			}
-			for (var i = 0; i < dnsZone.records.length; i++) {
-				var record = dnsZone.records[i];
-				if (dnsZone.records[i].name == name && dnsZone.records[i].type == type && dnsZone.records[i].data == data) {
-					if (dnsZone.records[i].ttl !== ttl) {
-						return callback(new Error('dns record already exists ttl change?'));
-					} else if (dnsZone.records[i].priority !== priority) {
-						return callback(new Error('dns record already exists priority change?'));
-					} else {
-						return callback(new Error('dns record already exists ' + type + ':' + name + ':' + data));
-					}
-				}
-			};
+            if (dnsZone.isNew) {
+                return reject(new restify.errors.NotFoundError('zone does not exists'));
+            }
 
-			record = new mongoose.DNSRecord({
-				name : name,
-				type : type,
-				data : data,
-				priority : priority,
-				reference : dnsZone._id
-			});
+            if (dns.validateRecord(dnsZone.records, name, type, data, priority, reject)) {
+                return
+            }
 
-			['ttl', 'admin', 'serial', 'refresh', 'retry', 'expiration', 'minimum'].forEach(function(key) {
-				if (_data.hasOwnProperty(key)) {
-					record[key] = _data[key];
-				}
-			});
+            let record = new mongoose.DNSRecord({
+                name: name,
+                type: type,
+                data: data,
+                priority: priority,
+                reference: dnsZone._id
+            });
 
-			record.save(function(err) {
-				if (err) {
-					err.type = 'InternalError';
-					return callback(err);
-				}
-				dnsZone.update({
-					'$push' : {
-						records : {
-							'$each' : [record._id]
-						}
-					}
-				}, function(err) {
-					if (err) {
-						err.type = 'InternalError';
-						return callback(err);
-					}
-					kue.dns.add(record.toRecord(), function(err, result) {
-						if (err) {
-							err.type = 'InternalError';
-							return callback(err);
-						}
-						callback(null, {
-							zone : dnsZone,
-							record : record,
-							raw : result
-						});
-					});
-				});
-			});
+            ['ttl', 'admin', 'serial', 'refresh', 'retry', 'expiration', 'minimum'].forEach(function (key) {
+                if (_data.hasOwnProperty(key)) {
+                    record[key] = _data[key];
+                }
+            });
 
-		});
-	},
-	remove : function(data, callback) {
-		var organization = data.organization;
+            try {
+                await Promise.all([record.save(), dnsZone.update({
+                    '$push': {
+                        records: {
+                            '$each': [record._id]
+                        }
+                    }
+                })]);
+                resolve({
+                    zone: dnsZone,
+                    record: record,
+                    raw: await kue.dns.add(record.toRecord())
+                });
+            } catch (err) {
+                err.type = 'InternalError';
+                return reject(err);
+            }
+        })
+    },
+    remove: function (data, callback) {
 
-		var name = data.name.toLowerCase();
-		var type = data.type.toUpperCase();
+        return new Promise(async function (resolve, reject) {
 
-		var data = data.data;
-		var ttl = data.ttl || 3600;
-		var zone = tld.getDomain(name.replace('*.', ''));
+            let organization = data.organization;
 
-		mongoose.DNSZone.findOne({
-			organization : organization,
-			zone : zone
-		}, function(err, dnsZone) {
-			if (err) {
-				err.type = 'InternalError';
-				return callback(err);
-			}
-			if (!dnsZone) {
-				return callback(Error('zone does not exists'));
-			}
-			if (dnsZone.isNew) {
-				return next(new restify.errors.NotFoundError('zone does not exists'));
-			}
-			for (var i = 0; i < dnsZone.records.length; i++) {
-				var record = dnsZone.records[i];
-				if (record.name == name && record.type == type && record.data == data) {
-					dnsZone.records.splice(i, 1);
-					break;
-				}
-				record = null;
-			};
+            let name = data.name.toLowerCase();
+            let type = data.type.toUpperCase();
 
-			if (!record) {
-				return callback(new Error('record does not exists'));
-			}
-			record.remove(function(err) {
-				if (err) {
-					err.type = 'InternalError';
-					return callback(err);
-				}
-				dnsZone.save(function(err) {
-					if (err) {
-						err.type = 'InternalError';
-						return callback(err);
-					}
+            let data = data.data;
+            let ttl = data.ttl || 3600;
+            let zone = tld.getDomain(name.replace('*.', ''));
+            let dnsZone
+            try {
+                dnsZone = await mongoose.DNSZone.findOne({
+                    organization: organization,
+                    zone: zone
+                })
+            } catch (err) {
+                err.type = 'InternalError';
+                return reject(err);
+            }
+            if (dnsZone.isNew) {
+                return next(new restify.errors.NotFoundError('zone does not exists'));
+            }
+            for (var record of dnsZone.records) {
+                if (record.name === name && record.type === type && record.data === data) {
+                    dnsZone.records.splice(i, 1);
+                    break;
+                }
+                record = null;
+            }
 
-					kue.dns.remove(record.toRecord(), function(err, result) {
-						if (err) {
-							err.type = 'InternalError';
-							return callback(err);
-						}
-						callback(null, {
-							zone : dnsZone,
-							record : record,
-							raw : result
-						});
-					});
-				});
-			});
+            if (!record) {
+                return reject(new Error('record does not exists'));
+            }
 
-		});
-	}
+            try {
+                await Promise.all([record.remove(), dnsZone.save()]);
+                resolve({
+                    zone: dnsZone,
+                    record: record,
+                    raw: await kue.dns.remove(record.toRecord())
+                });
+            } catch (err) {
+                err.type = 'InternalError';
+                return reject(err);
+            }
+        })
+
+
+        mongoose.DNSZone.findOne({
+            organization: organization,
+            zone: zone
+        }, function (err, dnsZone) {
+            if (err) {
+                err.type = 'InternalError';
+                return callback(err);
+            }
+            if (!dnsZone) {
+                return callback(Error('zone does not exists'));
+            }
+            if (dnsZone.isNew) {
+                return next(new restify.errors.NotFoundError('zone does not exists'));
+            }
+            for (var i = 0; i < dnsZone.records.length; i++) {
+                var record = dnsZone.records[i];
+                if (record.name == name && record.type == type && record.data == data) {
+                    dnsZone.records.splice(i, 1);
+                    break;
+                }
+                record = null;
+            }
+            ;
+
+            if (!record) {
+                return callback(new Error('record does not exists'));
+            }
+            record.remove(function (err) {
+                if (err) {
+                    err.type = 'InternalError';
+                    return callback(err);
+                }
+                dnsZone.save(function (err) {
+                    if (err) {
+                        err.type = 'InternalError';
+                        return callback(err);
+                    }
+
+                    kue.dns.remove(record.toRecord(), function (err, result) {
+                        if (err) {
+                            err.type = 'InternalError';
+                            return callback(err);
+                        }
+                        callback(null, {
+                            zone: dnsZone,
+                            record: record,
+                            raw: result
+                        });
+                    });
+                });
+            });
+
+        });
+    }
 };
---
+++ to.js
@@ -0,0 +1,6 @@
+exports = function to(promise) {
+    return promise.then(data => {
+        return [null, data];
+    })
+        .catch(err => [err]);
+}
